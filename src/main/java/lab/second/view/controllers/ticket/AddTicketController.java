bc27ccdd3c895c3e2d0836282f26f753f054f2bb
Vlad Bychkov <chapaev56@yandex.ru>
2020-03-02 05:12:26 +0400

    Edited call to DAOFactory

---
+++ src/main/java/lab/second/view/controllers/ticket/AddTicketController.java
@@ -0,0 +1,79 @@
+package lab.second.view.controllers.ticket;
+
+import airship.model.Airship;
+import airship.model.Client;
+import airship.model.Route;
+import airship.model.Ticket;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import lab.second.view.controllers.MainControl;
+
+import java.io.IOException;
+import java.net.URL;
+import java.rmi.RemoteException;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class AddTicketController extends MainControl implements Initializable {
+
+    static Client client;
+
+    @FXML
+    private Button mainRoutesButton;
+
+    @FXML
+    private Button mainTicketsButton;
+
+    @FXML
+    private Button mainClientsButton;
+
+    @FXML
+    private ComboBox<Route> routeChoiceBox;
+
+    @FXML
+    private ComboBox<Airship> airshipChoiceBox;
+
+    @FXML
+    private Button saveTicketButton;
+
+    @FXML
+    void saveTicketButtonAction(ActionEvent event) {
+        if (routeChoiceBox.getValue() == null || airshipChoiceBox.getValue() == null) {
+            Alert a = new Alert(Alert.AlertType.INFORMATION);
+            a.setTitle("Attention");
+            a.setHeaderText("Ops!");
+            String version = System.getProperty("java.version");
+            String content = String.format("The choice boxes should be not empty", version);
+            a.setContentText(content);
+            a.showAndWait();
+            return;
+        }
+            Ticket newTicket = new Ticket(airshipChoiceBox.getValue(), routeChoiceBox.getValue());
+            List<Ticket> tickets = client.getTickets();
+            tickets.add(newTicket);
+            client.setTickets(tickets);
+        try {
+            util.getClientDAO().add(client);
+            TicketListController.client = client;
+            toScene("ticket/list_tickets.fxml", "List Tickets", event);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            routeChoiceBox.setItems(FXCollections.observableArrayList(util.getRouteDAO().getList()));
+            airshipChoiceBox.setItems(FXCollections.observableArrayList(util.getAirshipDAO().getList()));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
