bebf25409e48a9d4ee17406528d9c291601ace36
Vlad Bychkov <chapaev56@yandex.ru>
2020-03-07 04:42:18 +0400

    Added class of alert dialog

---
+++ src/main/java/lab/second/view/controllers/DAOProvider.java
@@ -0,0 +1,46 @@
+package lab.second.view.controllers;
+
+import airship.dao.DAO;
+import airship.dao.FactoryDAO;
+import airship.model.Airship;
+import airship.model.Route;
+import airship.model.Ticket;
+import airship.model.Client;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+public class DAOProvider {
+
+    private static final String REMOTE_SERVICE_PATH = "rmi://localhost:5555/FactoryDAO";
+
+    private FactoryDAO factoryDAO;
+
+    public DAOProvider() {
+        try {
+            this.factoryDAO = (FactoryDAO) Naming.lookup(REMOTE_SERVICE_PATH);
+        } catch (NotBoundException | MalformedURLException | RemoteException e) {
+            AlertDialog.showErrorAlert(e);
+            e.printStackTrace();
+        }
+    }
+
+    public DAO<Airship> getAirshipDAO() throws RemoteException {
+        return factoryDAO.getAirshipDAO();
+    }
+
+    public DAO<Client> getClientDAO() throws RemoteException {
+        return factoryDAO.getClientDAO();
+    }
+
+    public DAO<Route> getRouteDAO() throws RemoteException {
+        return factoryDAO.getRouteDAO();
+    }
+
+    public DAO<Ticket> getTicketDAO() throws RemoteException {
+        return factoryDAO.getTicketDAO();
+    }
+
+}
